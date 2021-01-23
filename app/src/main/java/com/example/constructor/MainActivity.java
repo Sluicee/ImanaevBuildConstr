2284272f5cdd66b31dabdf93538603b1c1720f60
sluicee1 <sluicee1@gmail.com>
2021-01-23 18:12:16 +0400

    first

---
+++ app/src/main/java/com/example/constructor/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.constructor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View toolBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        toolBox = findViewById(R.id.toolbox);
+    }
+
+    public void showSettings(View v){
+        if (toolBox.getVisibility() == View.GONE)
+            toolBox.setVisibility(View.VISIBLE);
+        else
+            toolBox.setVisibility(View.GONE);
+    }
+
+}
